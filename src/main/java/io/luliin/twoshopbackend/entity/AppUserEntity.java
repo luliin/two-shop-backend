53098f4b4b2a5bd412d144103c5bd773db20b7dc
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-01-14 04:55:06 +0100

    Add eager fetching to Roles and shoppinglist items, with a fetch select to avoid multiple bag exception

--- src/main/java/io/luliin/twoshopbackend/entity/AppUserEntity.java
+++ src/main/java/io/luliin/twoshopbackend/entity/AppUserEntity.java
@@ -13,7 +13,6 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
