8d06e42bb7cdb649bc305f522735585fe6962bda
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-01-14 04:54:41 +0100

    Add eager fetching to Roles and shoppinglist items, with a fetch select to avoid multiple bag exception

--- src/main/java/io/luliin/twoshopbackend/entity/AppUserEntity.java
+++ src/main/java/io/luliin/twoshopbackend/entity/AppUserEntity.java
@@ -57,7 +57,7 @@ public class AppUserEntity implements UserDetails {
     private Timestamp createdAt;
     @Column(nullable = false)
     private Timestamp updatedAt;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     List<UserRole> roles = new ArrayList<>();
 
 
@@ -96,23 +96,34 @@ public class AppUserEntity implements UserDetails {
         }
     }
 
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
