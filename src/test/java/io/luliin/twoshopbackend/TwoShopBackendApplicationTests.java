326b63fb29ba337e45d57568a8e8e0e14cb8f2e0
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-01-20 09:54:09 +0100

    Set up test environment in TwoShopBackendApplicationTests

--- src/test/java/io/luliin/twoshopbackend/TwoShopBackendApplicationTests.java
+++ src/test/java/io/luliin/twoshopbackend/TwoShopBackendApplicationTests.java
@@ -2,12 +2,42 @@ package io.luliin.twoshopbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ContextConfiguration(initializers = TwoShopBackendApplicationTests.TwoShopApplicationTestsContextInitializer.class)
+@ActiveProfiles(value = "test")
 class TwoShopBackendApplicationTests {
+
+    @Container
+    private static final RabbitMQContainer rabbit = new RabbitMQContainer("rabbitmq:3.9.5");
+
 
     @Test
     void contextLoads() {
     }
 
+    public static class TwoShopApplicationTestsContextInitializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    configurableApplicationContext,
+                    "spring.rabbitmq.host=" + rabbit.getContainerIpAddress(), "spring.rabbitmq.port=" + rabbit.getMappedPort(5672));
+
+        }
+    }
+
 }
+
+
