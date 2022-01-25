4eca4e99768936e452841a30b18903f3cb8144d5
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-01-25 08:25:10 +0100

    Documentation added for all DTOs

--- src/main/java/io/luliin/twoshopbackend/dto/mail/UserPayload.java
+++ src/main/java/io/luliin/twoshopbackend/dto/mail/UserPayload.java
@@ -2,6 +2,11 @@ package io.luliin.twoshopbackend.dto.mail;
 
 /**
  * A DTO to use when populating content in dynamic email templates.
+ * @param username The recipient's username.
+ * @param email The recipient's email.
+ * @param firstName The recipient's first name.
+ * @param lastName The recipient's last name.
+ * @param updatedPassword The recipient's updated password (if update password mutation is called).
  * @author Julia Wigenstedt
  * Date: 2022-01-23
  */
