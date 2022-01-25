0044b68c00f0a6e1e4b185f2fc089dd028e9be60
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2022-01-25 08:52:54 +0100

    Documentation added for mail

--- src/main/java/io/luliin/twoshopbackend/mail/EmailResponse.java
+++ src/main/java/io/luliin/twoshopbackend/mail/EmailResponse.java
@@ -1,6 +1,8 @@
 package io.luliin.twoshopbackend.mail;
 
 /**
+ * This is a DTO received from mail service through message broker when an email has been sent.
+ * @param message The message from the mail service.
  * @author Julia Wigenstedt
  * Date: 2022-01-23
  */
